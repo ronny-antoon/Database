cd4b33ec7e200dd7591467ad1c8abe4e42bd9a07
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2023-10-27 04:09:57 +0300

    change env test github action

--- test/test_NVSDatabase/main.cpp
+++ test/test_NVSDatabase/main.cpp
@@ -1,37 +1,44 @@
 #ifndef RELEASE
+#ifdef EMBEDED
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Arduino.h>
 #include <gtest/gtest.h>
 
-// #include "PutPair_test.hpp"
-// #include "GetValueOf_test.hpp"
-// #include "IsExist_test.hpp"
-// #include "GetValueLengthOf_test.hpp"
-// #include "RemovePair_test.hpp"
-// #include "EraseAll_test.hpp"
+#include "PutPair_test.hpp"
+#include "GetValueOf_test.hpp"
+#include "IsExist_test.hpp"
+#include "GetValueLengthOf_test.hpp"
+#include "RemovePair_test.hpp"
+#include "EraseAll_test.hpp"
 
 void setup()
 {
-    // should be the same value as for the `test_speed` option in "platformio.ini"
-    // default value is test_speed=115200
     Serial.begin(115200);
-
     ::testing::InitGoogleTest();
-    // if you plan to use GMock, replace the line above with
-    // ::testing::InitGoogleMock();
 }
+
+void loop()
+{
+    if (RUN_ALL_TESTS())
+        ;
+
+    delay(1000);
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#else
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <gtest/gtest.h>
 
 TEST(Example, EmptyTest)
 {
     ASSERT_TRUE(true);
 }
 
-void loop()
+int main(int argc, char **argv)
 {
-    // Run tests
-    if (RUN_ALL_TESTS())
-        ;
-
-    // sleep for 1 sec
-    delay(1000);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#endif
 #endif
